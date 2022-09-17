3483ca2fc323e908b462ebe49ac808ad698a2601
Gustavo <gs.lanauze@gmail.com>
2022-09-16 23:28:29 -0400

    First Commit

---
+++ java.js
@@ -0,0 +1,33 @@
+
+
+// counter function
+function add() {
+    // console.log(UP);
+    // document.querySelector(".mainNum").innerHTML ++ ;
+    document.querySelector(".mainNum").innerHTML++; 
+
+}
+
+// high score function
+function submit() {
+var name = prompt("What is your Name"); 
+document.querySelector(".scorez").innerHTML= "1." + " " + name + " - " + " " + document.querySelector(".mainNum").innerHTML;
+}
+
+
+// if (.mainNum >= 50) {
+//     num += 5; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
